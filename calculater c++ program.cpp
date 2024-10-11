eb6860293ddcc5d7614fc5a7f1236501a624dbe4
tashfenshah01 <tashfens@gmail.com>
2024-10-11 22:56:45 +0500

    Add files via upload

---
+++ calculater c++ program.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// held the variables
+double num1, num2;
+char operation;
+
+// allow the usern to enter two numbers
+cout <<"Enter first number: ";
+cin >> num1;
+cout << "Enter the second number: ";
+cin>> num2;
+
+// allow the user to use an operation
+cout << "Enter an operation (+,-,*,/): ";
+cin >> operation;
+
+
+// Perform chosen operation switch(operation) {
+    case '+:'
+    cout <<"Result: " <<
+   num1+num2 << endl;
+    break;         
+    
+
+
+
+ 
+}
+ 
+ 
+ 
+ 
+ 
